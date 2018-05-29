c56d2f7e1b7a263170a567498f9ee5acff83752b
king-kong <yangqp5@outlook.com>
2018-05-29 13:13:18 +0800

    some update

--- include/math/math.hpp
+++ include/math/math.hpp
@@ -7,6 +7,41 @@ template <typename N>
 inline bool odd(N n) {
   return (n & 0x1);
 }
+
+/*
+ * f could be the operation representing addition or multiplication
+ */
+template <typename R, typename N, typename BinaryOperation>
+R power_recursive1(R acc, R Base, N n, BinaryOperation f) {
+  // Haven't thought of a good way to provide an identity element
+  if ( n < 1 ) throw;
+
+  if ( n == 1 ) return acc;
+  if ( odd(n) ) {
+    return power_recursive(f(acc, Base), f(Base, Base), n >> 1, f);
+  }
+  else {
+    return power_recursive(acc, f(Base, Base), n >> 1, f);
+  }
+}
+
+/*
+ * Strict tail recursion
+ */
+template <typename R, typename N, typename BinaryOperation>
+R power_recursive2(R acc, R Base, N n, BinaryOperation f) {
+  // Haven't thought of a good way to provide an identity element
+  if ( n < 1 ) throw;
+
+  if ( odd(n) ) {
+    acc = f(acc, Base);
+    if ( n == 1 ) return acc;
+  }
+  Base = f(Base, Base);
+  n >>= 1;
+  return power_recursive(acc, Base, n, f);
+}
+
 /*
  * A generic power function
  * Only accept n >= 1
